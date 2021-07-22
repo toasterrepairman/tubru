beaf48e604923905c913cd7055d6faa44b0ccb12
toast <smol@toast.cyou>
2021-07-22 17:02:35 -0400

    added more commentary for real this time

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,11 @@
 use fltk::{app::*, enums::*, frame::*, input::*, prelude::*, button::*, window::*};
 use rustube;
 use fltk::text::{TextBuffer, SimpleTerminal};
-
 // imported fltk and rustube dependencies
 
 const WIDGET_HEIGHT: i32 = 50;
 const WIDGET_PADDING: i32 = 10;
 const WIDGET_WIDTH: i32 = 200;
-
 // establish widget dimensions
 
 #[derive(Clone, Copy)]
@@ -16,7 +14,6 @@ enum Message {
     DownloadReady,
     DownloadFinished,
 }
-
 // create state enums
 
 fn main() {
@@ -27,41 +24,49 @@ fn main() {
             WIDGET_HEIGHT * 2 + WIDGET_PADDING,
         )
         .with_label("tubru");
+        // initializing program window
 
     let (sender, reciever) = channel::<Message>();
+    // open channel for Message enum
 
     let mut link_input = Input::default()
         .with_size(WIDGET_WIDTH * 2 + (WIDGET_PADDING * 2), WIDGET_HEIGHT / 2)
         .with_pos(WIDGET_PADDING, WIDGET_PADDING);
     link_input.set_trigger(CallbackTrigger::Changed);
     link_input.emit(sender, Message::HasLink);
+    // input window creation
 
     let mut download = Button::default()
         .with_size(WIDGET_WIDTH, WIDGET_HEIGHT)
         .below_of(&link_input, WIDGET_PADDING)
         .with_label("download");
     download.emit(sender, Message::DownloadReady);
- 
+    // download button creation
+
     let mut status = SimpleTerminal::default()
         .with_size(WIDGET_WIDTH + WIDGET_PADDING, WIDGET_HEIGHT)
         .right_of(&download, WIDGET_PADDING);
+    // status box creation
 
     download.deactivate();
+    // deactivate button so state can handle it
 
     link_input.set_color(Color::from_u32(0x2e3440));
     link_input.set_text_color(Color::from_u32(0xeceff4));
     link_input.set_selection_color(Color::from_u32(0x5e81ac));
-
+    
     download.set_color(Color::from_u32(0x4c566a));
     download.set_label_size(32);
 
     wind.set_color(Color::from_u32(0x353c4a));
+    // theming the program uwu
 
     wind.end();
     wind.show();
     while app.wait() {
+        // initialize state loop
         match reciever.recv() {
-            Some(Message::HasLink) => {
+            Some(Message::HasLink) => { // link access state
                 status.set_text("Press enter to start download");
                 if &link_input.value() != "" {
                     download.activate();
@@ -69,20 +74,20 @@ fn main() {
                     download.deactivate();
                 }
             }
-            Some(Message::DownloadReady) => {
+            Some(Message::DownloadReady) => { // download press state
                 link_input.deactivate();
                 status.set_text("Download in progress...");
 
                 let link: String = link_input.value();
-                
+
                 video_downloadfn(&link);
 
                 link_input.emit(sender, Message::DownloadFinished);
             }
-            Some(Message::DownloadFinished) => {
+            Some(Message::DownloadFinished) => { // download finished state
                 // empty for now
             }
-            None => {}
+            None => {} // else handler
         }
     }
 }
